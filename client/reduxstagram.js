5c72a42b7b1e5254bc896453c85dcf31a390c608
Annie Chiang <devbear22@gmail.com>
2017-12-20 15:44:27 -0800

    render title from main component

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,14 @@
-// let's go!
+import React from 'react';
+
+import { render } from 'react-dom';
+
+// import css
+import css from './styles/style.styl';
+
+// import components
+import Main from './components/Main';
+
+// good to see if something is rendering in general 
+render(<Main />, document.getElementById('root'));
+
+
