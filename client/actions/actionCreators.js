b169163d360e5e35b70a49b6abe023f614794dcf
Annie Chiang <devbear22@gmail.com>
2017-12-20 17:20:09 -0800

    setup actions for app

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,35 @@
+
+// increment likes - index = which post
+const INCREMENT_LIKES = 'INCREMENT_LIKES';
+export function increment(index) {
+  return {
+    type: INCREMENT_LIKES,
+    payload: {
+      index
+    }
+  }
+}
+// add comment
+const ADD_COMMENT = 'ADD_COMMENT';
+export function addComment(postId, author, comment) {
+  return {
+    type: ADD_COMMENT,
+    payload: {
+      postId,
+      author,
+      comment
+    }
+  }
+}
+// remove comment - find the id in the index
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export function removeComment(postId, index) {
+  return {
+    type: REMOVE_COMMENT,
+    payload: {
+      index,
+      postId
+    }
+  }
+}
+
