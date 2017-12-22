209a33604eb88716e42159f27e97f10111ad94a3
Annie Chiang <devbear22@gmail.com>
2017-12-22 11:04:44 -0800

    fix typo

--- client/components/Single.js
+++ client/components/Single.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Single extends React.Component {
   render() {
     return (
-      <div clasName = "single-photo">
+      <div className = "single-photo">
         <p> Single photo! </p>
       </div>
     )
