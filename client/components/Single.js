4bde0be38fc4e1c4734a52fa8a71acbd88828030
Annie Chiang <devbear22@gmail.com>
2017-12-22 15:20:15 -0800

    display and add comment section

--- client/components/Single.js
+++ client/components/Single.js
@@ -6,14 +6,15 @@ export default class Single extends React.Component {
   render() {
     // need index of the post
     // findIndex - loop over array until postId found
-    const index = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const { postId } = this.props.params;
+    const index = this.props.posts.findIndex((post) => post.code === postId);
     const post = this.props.posts[index];
-    console.log('post ->', post);
-    // get the post
+    const postComments = this.props.comments[postId] || []; 
+    
     return (
       <div className = "single-photo">
         <Photo index={index} post={post} {...this.props} />
-        <Comments />
+        <Comments postComments={postComments}/>
       </div>
     )
   }
